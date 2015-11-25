9e045b08dffd1098274cd588b5242499da833832
Lumen <credeville.louis.maxime@gmail.com>
2015-11-25 19:45:40 +0100

    ras

--- src/gestion_spectacle/SeanceCinema.java
+++ src/gestion_spectacle/SeanceCinema.java
@@ -4,20 +4,16 @@ public class SeanceCinema extends Seance {
 
     private Salle salle;
     private int nbPlacesVenduesTR;
-    
+
     @Override
     public int nbPlacesDispo() {
-<<<<<<< HEAD
-	// TODO Auto-generated method stub //chat
-=======
-	// TODO Auto-generated method stub //chien
->>>>>>> branch 'master' of https://github.com/Lumenol/Projet_JAVA.git
+	// TODO Auto-generated method stub
 	return 0;
     }
 
     @Override
     public int totalCendu() {
-	// TODO Auto-generated method stub //git
+	// TODO Auto-generated method stub
 	return 0;
     }
 
@@ -27,8 +23,8 @@ public class SeanceCinema extends Seance {
 	return 0;
     }
 
-    public void vendrePlacesTR(int nbre){
-	
+    public void vendrePlacesTR(int nbre) {
+
     }
-    
+
 }
